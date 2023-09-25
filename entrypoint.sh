c835e37a2e732918f672a9a918590eb34861fd50
WchrpWwtk <wchrp.wwtk@gmail.com>
2023-09-26 02:00:49 +0700

    Add command `python wait_for_db.py` in `entrypoint.sh`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "${0}: running migrations."
+python wait_for_db.py
 python manage.py makemigrations --merge
 python manage.py migrate --noinput
 python manage.py runserver 0.0.0.0:8000
